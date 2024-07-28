c1c9afd74d1c6b334c19aa5390bd4e1f3ee527f6
Hubert Sajdak <hub.sajdak@gmail.com>
2024-07-28 14:09:39 +0200

    feat: create-layout

--- libs/shared/ui/nav-link/src/lib/nav-link.spec.tsx
+++ libs/shared/ui/nav-link/src/lib/nav-link.spec.tsx
@@ -1,10 +1,10 @@
 import { render } from '@testing-library/react';
 
-import NavLink from './nav-link';
+import { NavLink } from './nav-link';
 
 describe('SharedUiNavLink', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<NavLink />);
+    const { baseElement } = render(<NavLink label={'test'} path={''} />);
     expect(baseElement).toBeTruthy();
   });
 });
