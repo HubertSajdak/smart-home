646a991ec57e6a9184d857842a8d325614113c27
Hubert Sajdak <hub.sajdak@gmail.com>
2024-07-29 10:30:27 +0200

    fix: remove empty test

--- libs/device/ui-room-list/src/lib/device-ui-room-list.spec.tsx
+++
@@ -1,8 +0,0 @@
-import { DeviceUiRoomList } from './device-ui-room-list';
-
-describe('DeviceUiRoomList', () => {
-  // it('should render successfully', () => {
-  //   const { baseElement } = render(<DeviceUiRoomList />);
-  //   expect(baseElement).toBeTruthy();
-  // });
-});
