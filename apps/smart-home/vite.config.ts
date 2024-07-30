2448d09f7c39da38c682f32319b89a2686711164
Hubert Sajdak <hub.sajdak@gmail.com>
2024-07-30 23:17:58 +0200

    fix: pass with empty tests

--- apps/smart-home/vite.config.ts
+++ apps/smart-home/vite.config.ts
@@ -39,15 +39,4 @@ export default defineConfig({
       transformMixedEsModules: true,
     },
   },
-
-  test: {
-    watch: false,
-    globals: true,
-    cache: { dir: '../../../node_modules/.vitest/apps/smart-home' },
-    environment: 'jsdom',
-    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-    reporters: ['default'],
-    coverage: { reportsDirectory: '../../../coverage/apps/smart-home', provider: 'v8' },
-    passWithNoTests: true,
-  },
 });
